699afdba91fe458c0e3d15d423697bab47c26d4d
Subhas <subhas.sing@gmail.com>
2017-07-26 22:57:11 -0700

    docker container permission denied

--- start_docker.sh
+++ start_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sh docker stop $(docker ps -a -q -f "ancestor=jenkins-pipeline") >> /dev/null
-sh docker rm $(docker ps -a -q -f "ancestor=jenkins-pipeline") >> /dev/null
+docker stop $(docker ps -a -q -f "ancestor=jenkins-pipeline") >> /dev/null
+docker rm $(docker ps -a -q -f "ancestor=jenkins-pipeline") >> /dev/null
 
-sh docker build -t jenkins-pipeline .
+docker build -t jenkins-pipeline .
 # - local post : container port
-sh docker run -d -p 9092:8080 jenkins-pipeline  >> pid.txt
+docker run -d -p 9092:8080 jenkins-pipeline  >> pid.txt
